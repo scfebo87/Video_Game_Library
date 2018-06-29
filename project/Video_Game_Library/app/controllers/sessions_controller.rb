992ef79144347e05e879317757aff488617dd333
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-06-28 19:00:34 -0700

    added new and create methods in sessions controller for logging in.

--- project/Video_Game_Library/app/controllers/sessions_controller.rb
+++ project/Video_Game_Library/app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+     def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+     end
 end
