c5a34bcaa10f429b344fd9b4c14f162da308b14e
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-06-29 14:04:27 -0700

    fixed user_params and and added session[:user_id] to create method for signing up.

--- project/Video_Game_Library/app/controllers/users_controller.rb
+++ project/Video_Game_Library/app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
@@ -21,11 +22,10 @@ class UsersController < ApplicationController
             redirect_to root_path
         end
     end
-    end
 
     private
 
     def user_params
-        params.require(:user).permit(:user_name, :password, :password)
+        params.require(:user).permit(:user_name, :email, :password)
     end
 end
