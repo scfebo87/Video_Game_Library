4a5f54049b91c0677a4bbfe92989617b637dae24
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-06-30 14:39:20 -0700

    added instance to index. simplified new method. added show method.

--- project/Video_Game_Library/app/controllers/consoles_controller.rb
+++ project/Video_Game_Library/app/controllers/consoles_controller.rb
@@ -1,24 +1,30 @@
 class ConsolesController < ApplicationController
 
     def index
+        @consoles = Console.all
     end
 
     def new
-        if params[:user_id]
-            @console = Console.new
-        else
-            redirect_to root_path
-        end
+        @console = Console.new
     end
 
     def create
-        @user = User.find(params[:id])
+        @user = User.find_by(id: session[:user_id])
         @console = Console.new(console_params)
         if @console.save
-            @user.consoles << @console
             redirect_to user_path(@user)
         else
-            render :new
+            redirect_to new_console_path
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:user_id])
+         @console = Console.find(params[:id])
+        if @user && @user.id == session[:user_id]
+            render :show
+        else
+            redirect_to user_path(@user)
         end
     end
 
