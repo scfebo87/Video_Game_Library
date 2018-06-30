349f6b7e923bc8d069d81367879abaff24a25424
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-06-30 14:40:06 -0700

    fixed routes.

--- project/Video_Game_Library/config/routes.rb
+++ project/Video_Game_Library/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#index'
     resources :users do
-        resources :consoles, only: [:new, :show]
+        resources :consoles, only: [:index, :show]
     end
-    resources :consoles, only: [:index]
+    resources :consoles
     get '/login' => 'sessions#new'
     post '/login' => 'sessions#create'
 end
