147281a5b60318b3648f95add7de1fe413f795e5
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-06-30 11:26:29 -0700

    added routes for users' consoles.

--- project/Video_Game_Library/config/routes.rb
+++ project/Video_Game_Library/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#index'
-    resources :users
+    resources :users do
+        resources :consoles
+    end
     get '/login' => 'sessions#new'
     post '/login' => 'sessions#create'
 end
