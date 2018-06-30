418f2fa9aa687c864d7615f457c3e8caba132456
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-06-30 12:12:55 -0700

    added show route.

--- project/Video_Game_Library/config/routes.rb
+++ project/Video_Game_Library/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#index'
     resources :users do
-        resources :consoles, only: [:new]
+        resources :consoles, only: [:new, :show]
     end
     resources :consoles, only: [:index]
     get '/login' => 'sessions#new'
