a1478fbba8808b00090f8ef19f7b7df3be5e0564
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-06-30 11:25:18 -0700

    created consoles controller.

---
+++ project/Video_Game_Library/app/controllers/consoles_controller.rb
@@ -0,0 +1,28 @@
+class ConsolesController < ApplicationController
+
+    def index
+    end
+
+    def new
+        if params[:user_id]
+            @console = Console.new
+        else
+            redirect_to root_path
+        end
+    end
+
+    def create
+        @user = User.find(params[:id])
+        @console = Console.new(console_params)
+        if @console.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def console_params
+        params.require(:console).permit(:company, :name, :model)
+end
