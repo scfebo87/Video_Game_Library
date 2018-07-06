c8c20f3d4dde718537e8ae31e05cccf0791603cd
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-07-06 14:04:27 -0700

    added routes for games.

--- project/Video_Game_Library/config/routes.rb
+++ project/Video_Game_Library/config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#index'
     resources :users do
-        resources :consoles, only: [:index, :show, :new, :edit]
+        resources :consoles, only: [:index, :show, :new, :edit] do
+            resources :games, only: [:index, :show, :new, :edit]
+        end
     end
     resources :consoles
+    resources :games
     get '/login' => 'sessions#new'
     post '/login' => 'sessions#create'
 end
