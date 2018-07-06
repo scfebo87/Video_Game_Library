9e953483f2c81e0288c6a9d9c7ae644c2dadcbd0
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-07-06 10:27:02 -0700

    updated nested routes.

--- project/Video_Game_Library/config/routes.rb
+++ project/Video_Game_Library/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#index'
     resources :users do
-        resources :consoles, only: [:index, :show]
+        resources :consoles, only: [:index, :show, :new, :edit]
     end
-    resources :consoles
+    resources :consoles, only: [:index]
     get '/login' => 'sessions#new'
     post '/login' => 'sessions#create'
 end
