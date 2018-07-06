a727e8c3e841ee21c3b622a18001dd1aa5d30899
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-07-06 13:06:06 -0700

    updated index method for users logged in.

--- project/Video_Game_Library/app/controllers/application_controller.rb
+++ project/Video_Game_Library/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    def index
+        @user = User.find(session[:user_id])
+        if @user && @user.id = session[:user_id]
+            redirect_to user_path(@user)
+        else
+            redirect_to root_path
+        end
+    end
 end
