9b4039fc79e7529c970dd49b55d92cfc183f3f15
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-07-10 12:36:34 -0700

    cleaned up code by adding method require_login to make sure user is logged in before viewing pages.

--- project/Video_Game_Library/app/controllers/consoles_controller.rb
+++ project/Video_Game_Library/app/controllers/consoles_controller.rb
@@ -1,24 +1,12 @@
 class ConsolesController < ApplicationController
+    before_action :require_login
 
     def index
-        if params[:user_id]
-            @user = User.find(params[:user_id])
-                if @user && @user.id == session[:user_id]
-                    @consoles = @user.consoles
-            else
-                redirect_to root_path
-            end
-        else
-            redirect_to root_path
-        end
+        @user = User.find(params[:user_id])
+        @consoles = @user.consoles
     end
 
     def new
-        if params[:user_id]
-            @console = Console.new
-        else
-            redirect_to root_path
-        end
     end
 
     def create
@@ -34,21 +22,13 @@ class ConsolesController < ApplicationController
     def show
         @console = Console.find(params[:id])
         @user = User.find(params[:user_id])
-        if @user && @console && @user.id == session[:user_id]
-            @consoles = @user.consoles
-        else
-            redirect_to root_path
-        end
+        @consoles = @user.consoles
     end
 
 
     def edit
-        if params[:user_id]
-            @console = Console.find(params[:id])
-            @user = User.find(params[:user_id])
-        else
-            redirect_to user_consoles_path(@user)
-        end
+        @console = Console.find(params[:id])
+        @user = User.find(params[:user_id])
     end
 
     def update
@@ -73,4 +53,8 @@ class ConsolesController < ApplicationController
     def console_params
         params.require(:console).permit(:company, :name, :model)
     end
+
+    def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
 end
