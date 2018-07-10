0470bb682a0f7ef19bf1c139de9ee8b3daf370f6
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-07-09 23:36:27 -0700

    temp fix for what signed in users see.

--- project/Video_Game_Library/app/controllers/application_controller.rb
+++ project/Video_Game_Library/app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
 
     def index
+        if params[:id]
         @user = User.find(session[:user_id])
         if @user && @user.id = session[:user_id]
             redirect_to user_path(@user)
         else
-            redirect_to root_path
+            render :index
+        end
+        else
+            render :index
         end
     end
 end
