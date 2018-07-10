27d4b34dbc10c98337f5e80a31c1cce15798363d
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-07-09 23:37:37 -0700

    temp fix for what loggedin users see. added button to add new games.

--- project/Video_Game_Library/app/controllers/sessions_controller.rb
+++ project/Video_Game_Library/app/controllers/sessions_controller.rb
@@ -1,15 +1,26 @@
 class SessionsController < ApplicationController
 
     def new
+        @user = User.find_by(id: params[:user_id])
+        if @user && @user.id == session[:user_id]
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
-     def create
-        @user = User.find_by(name: params[:user][:name])
-        if @user && @user.authenticate(params[:user][:password])
+    def create
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password_digest])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
         end
      end
+
+     def destroy
+        session.clear
+        redirect_to '/'
+    end
 end
--- project/Video_Game_Library/app/controllers/users_controller.rb
+++ project/Video_Game_Library/app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
     def new
-        @user = User.new
+         @user = User.find_by(id: params[:user_id])
+         if @user && @user.id == session[:user_id]
+            redirect_to user_path(@user)
+        else
+            @user = User.new
+         end
     end
 
     def create
