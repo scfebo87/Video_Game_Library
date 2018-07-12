a85771d1703b0988f4525114fc628166bb6fcfc3
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-07-11 21:36:43 -0700

    deleted repeated code.

--- project/Video_Game_Library/app/controllers/application_controller.rb
+++ project/Video_Game_Library/app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
 
     def index
-        if params[:id]
-        @user = User.find(session[:user_id])
-        if @user && @user.id = session[:user_id]
+        @user = User.find_by(id: session[:user_id])
+        if @user && @user.id == session[:user_id]
             redirect_to user_path(@user)
-        else
-            render :index
-        end
         else
             render :index
         end
