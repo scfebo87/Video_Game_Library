31ae24554e4a0603397fa96574c65e9fe1c0e174
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-07-17 23:25:07 -0700

    added method to Console to find which has the most games for that user.

--- project/Video_Game_Library/app/models/console.rb
+++ project/Video_Game_Library/app/models/console.rb
@@ -2,4 +2,12 @@ class Console < ActiveRecord::Base
     has_many :games
     has_many :users, through: :games
     validates :name, :model, :company, presence: true
+
+
+    def self.most_games
+        self.order( "games_count desc" ).limit(1).all
+    end
+
+
+
 end
