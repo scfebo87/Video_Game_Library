5353331e6164203a424c61a6a3f90199b1502619
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-11-30 22:15:31 -0800

    changed url for game.

--- project/Video_Game_Library/app/assets/javascripts/game.js
+++ project/Video_Game_Library/app/assets/javascripts/game.js
@@ -6,7 +6,7 @@ $(function () {
   $(".js-next").on("click", function() {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
     $.get("/users/:user_id/consoles/:console_id/games/" + nextId + ".json", function(json) {
-      $("#gameTitle").html("<a href=" + json["console_id"] + "/games/" + json["id"]+ ">" + json["title"] + "</a>")
+      $("#gameTitle").html("<a href=" + json["console_id"] + "/games/" + json["id"] + "/edit >" + json["title"] + "</a>")
       $(".js-next").attr("data-id", json["id"]);
     });
   });
